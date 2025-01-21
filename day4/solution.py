cd04a052419d7c4dbff3f8c5f15402d73bc27909
Vanilla1002 <Shukerharel@gmail.com>
2025-01-21 18:42:42 +0200

    day4 p2

--- day4/solution.py
+++ day4/solution.py
@@ -40,17 +40,62 @@ def helper(row, col, word, array):
     return count
     
 
-def part1(first_letter, word,array):
+def part2_helper(row, col, word, array):
+    directions = [
+        (1, 1),   # Down-Right
+        ]
+    count = 0 
+    rows = len(array)
+    cols = len(array[0])
+    length = len(word)
+    for x, y in directions:
+        is_good = True
+        for index in range(length):
+            new_row = row + x * index
+            new_col = col + y * index
+            if not (0 <= new_row < rows and 0 <= new_col < cols) or array[new_row][new_col] != word[index]:
+                is_good = False
+                break
+        if is_good:
+            if set([array[row + x*2][col], array[row][col + y*2]]) == {'S', 'M'}:
+                count += 1
+    word = word[::-1]
+    for x, y in directions:
+        is_good = True
+        for index in range(length):
+            new_row = row + x * index
+            new_col = col + y * index
+            if not (0 <= new_row < rows and 0 <= new_col < cols) or array[new_row][new_col] != word[index]:
+                is_good = False
+                break
+        if is_good:
+            if set([array[row + x*2][col], array[row][col + y*2]]) == {'S', 'M'}:
+                count += 1
+    return count
+    
+
+def part1( word,array):
     result = 0
     rows = len(array)
     cols = len(array[0])
     for i in range(rows):
         for j in range(cols):
-            if array[i][j] == first_letter:
-                result += helper(i, j, word, array)
+            result += helper(i, j, word, array)
 
     return result
 
+
+
+def part2(word,array):
+    result = 0
+    rows = len(array)
+    cols = len(array[0])
+    for i in range(rows):
+        for j in range(cols):
+            result += part2_helper(i, j, word, array)
+    return result
+
+                
 
             
 
@@ -60,5 +105,5 @@ def part1(first_letter, word,array):
 if __name__ == "__main__":
     input_file = './day4/input4.txt'
     listof= process_input(input_file)
-    x=part1('X', 'XMAS', listof)
-    print(x)
+    print(part1('XMAS', listof))
+    print(part2('MAS', listof))
